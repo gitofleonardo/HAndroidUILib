cd40061e8c943fce9ce2ce5a0017c279e776a2dc
huangchengxi <huangchengxi@iboxpay.com>
2021-09-30 12:28:08 +0800

    Finish the fucking OverScrollView.kt

--- UILib/src/main/java/cn/huangchengxi/uilib/container/overscroll/OverScrollView.kt
+++ UILib/src/main/java/cn/huangchengxi/uilib/container/overscroll/OverScrollView.kt
@@ -8,15 +8,25 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.VelocityTracker
 import android.view.View
+import android.view.View.MeasureSpec
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
 import android.widget.FrameLayout
+import android.widget.HorizontalScrollView
 import android.widget.OverScroller
 import android.widget.Scroller
+import cn.huangchengxi.uilib.R
+import kotlin.math.abs
+import kotlin.math.log10
+import kotlin.math.log2
+import kotlin.math.sqrt
 
 /**
  * @author HuangChengxi
  * This view is an extending view of {@see android.widget.ScrollView}, which supports
  * overscroll.
+ *
+ * UGLY CODE
  */
 class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:Int):FrameLayout(context, attrs,defStyle,defRes){
     constructor(context: Context,attrs: AttributeSet?,defStyle: Int):this(context,attrs,defStyle,-1)
@@ -24,7 +34,7 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
     constructor(context: Context):this(context,null)
 
     private val TAG="OverScrollView"
-    private val mOverScroller = Scroller(context)
+    private val mOverScroller = OverScroller(context)
     private var mTracker:VelocityTracker?=null
     private var mLastX=0
     private var mLastY=0
@@ -102,7 +112,21 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
     /**
      * Supports scroll direction
      */
-    private var mScrollDirection:ScrollDirection=ScrollDirection.VERTICAL
+    private var mScrollDirection:ScrollDirection=ScrollDirection.HORIZONTAL
+
+    init {
+        val arr=context.obtainStyledAttributes(attrs,R.styleable.OverScrollView)
+        val direction=arr.getInt(R.styleable.OverScrollView_direction,0)
+        mScrollDirection = if (direction==0){
+            ScrollDirection.VERTICAL
+        }else if (direction==1){
+            ScrollDirection.HORIZONTAL
+        }else{
+            throw IllegalArgumentException("Wrong direction type")
+        }
+        arr.recycle()
+        clipChildren=false
+    }
 
     /**
      * We want user to define his own layout style,
@@ -160,12 +184,60 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
             }
             val desiredHeight=measuredHeight-heightPadding
             val desiredWidth=measuredWidth-widthPadding
-            val childWidthSpec= MeasureSpec.makeMeasureSpec(desiredWidth,MeasureSpec.EXACTLY)
-            val childHeightSpec= MeasureSpec.makeMeasureSpec(desiredHeight,MeasureSpec.EXACTLY)
-            Log.v(TAG,"Child measure width height before:${child.measuredWidth},${child.measuredHeight}")
-            child.measure(childWidthSpec,desiredHeight)
-            Log.v(TAG,"Child measure width height after:${child.measuredWidth},${child.measuredHeight}")
+            when(mScrollDirection){
+                ScrollDirection.VERTICAL->{
+                    if (child.measuredHeight<desiredHeight){
+                        val widthSpec= getChildMeasureSpec(widthMeasureSpec,widthPadding,params.width)
+                        val heightSpec=MeasureSpec.makeMeasureSpec(desiredHeight,MeasureSpec.UNSPECIFIED)
+                        child.measure(widthSpec,heightSpec)
+                    }
+                }
+                ScrollDirection.HORIZONTAL->{
+                    if (child.measuredWidth<desiredWidth){
+                        val widthSpec=MeasureSpec.makeMeasureSpec(desiredWidth,MeasureSpec.UNSPECIFIED)
+                        val heightSpec= getChildMeasureSpec(heightMeasureSpec,heightPadding,params.height)
+                        child.measure(widthSpec,heightSpec)
+                    }
+                }
+            }
         }
+    }
+
+    override fun measureChild(
+        child: View, parentWidthMeasureSpec: Int,
+        parentHeightMeasureSpec: Int
+    ) {
+        val lp = child.layoutParams
+        val childWidthMeasureSpec: Int
+        val childHeightMeasureSpec: Int
+        childWidthMeasureSpec = getChildMeasureSpec(
+            parentWidthMeasureSpec, paddingLeft
+                    + paddingRight, lp.width
+        )
+        val verticalPadding: Int = paddingTop + paddingBottom
+        childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+            Math.max(0, MeasureSpec.getSize(parentHeightMeasureSpec) - verticalPadding),
+            MeasureSpec.UNSPECIFIED
+        )
+        child.measure(childWidthMeasureSpec, childHeightMeasureSpec)
+    }
+    override fun measureChildWithMargins(
+        child: View, parentWidthMeasureSpec: Int, widthUsed: Int,
+        parentHeightMeasureSpec: Int, heightUsed: Int
+    ) {
+        val lp = child.layoutParams as MarginLayoutParams
+        val childWidthMeasureSpec = getChildMeasureSpec(
+            parentWidthMeasureSpec,
+            paddingLeft + paddingRight + lp.leftMargin + lp.rightMargin
+                    + widthUsed, lp.width
+        )
+        val usedTotal: Int = (paddingTop + paddingBottom + lp.topMargin + lp.bottomMargin +
+                heightUsed)
+        val childHeightMeasureSpec: Int = MeasureSpec.makeMeasureSpec(
+            Math.max(0, MeasureSpec.getSize(parentHeightMeasureSpec) - usedTotal),
+            MeasureSpec.UNSPECIFIED
+        )
+        child.measure(childWidthMeasureSpec, childHeightMeasureSpec)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -178,6 +250,7 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
             MotionEvent.ACTION_DOWN->{
                 //Cancel all the animators
                 cancelAllRestoreAnimator()
+                mOverScroller.forceFinished(true)
                 mTracker= VelocityTracker.obtain()
                 mLastX=curX
                 mLastY=curY
@@ -195,19 +268,16 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
                             return true
                         }
                     }
-                    ScrollDirection.ALL->{
-                        if (mLastX==curX && mLastY==curY){
-                            return true
-                        }
-                    }
                 }
-                val moveX=mLastX-curX
-                val moveY=mLastY-curY
-                //Log.v(TAG,"Move:${moveX},${moveY}")
+                var moveX=mLastX-curX
+                var moveY=mLastY-curY
+                if (mHorizontalAlreadyOverScroll){
+                    moveX=computeOverScrollValue(moveX)
+                }
+                if (mVerticalAlreadyOverScroll){
+                    moveY=computeOverScrollValue(moveY)
+                }
                 when (mScrollDirection){
-                    ScrollDirection.ALL->{
-                        scrollBy(moveX,moveY)
-                    }
                     ScrollDirection.VERTICAL->{
                         scrollBy(0,moveY)
                     }
@@ -216,7 +286,6 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
                     }
                 }
                 mTracker?.addMovement(event)
-                mTracker?.computeCurrentVelocity(1000)
                 mLastX=curX
                 mLastY=curY
                 return true
@@ -231,9 +300,12 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
                 if (mHorizontalAlreadyOverScroll && mScrollDirection!=ScrollDirection.VERTICAL){
                     scheduleHorizontalRestoreAnimator()
                 }
+                mTracker?.computeCurrentVelocity(1000)
                 val velocityX=mTracker?.xVelocity?.toInt()?:0
                 val velocityY=mTracker?.yVelocity?.toInt()?:0
                 processFling(velocityX,velocityY)
+                mTracker?.recycle()
+                mTracker=null
                 return true
             }
             else->{
@@ -241,27 +313,29 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
             }
         }
     }
-
-    /**
-     * 说的什么鸡巴玩意这个傻逼scroller
-     * 注释说的是什么鸡巴东西不清不楚傻逼东西
-     */
+    private fun computeOverScrollValue(distance:Int):Int{
+        return distance/2
+    }
     private fun processFling(xVelocity:Int,yVelocity:Int){
         var xv=xVelocity
         var yv=yVelocity
-        xv=if (xv>=MIN_FLING_VELOCITY) xv else 0
-        yv=if (yv>=MIN_FLING_VELOCITY) yv else 0
-        val availableVerticalScroll=mAvailableScrollAreaHeight-mVisibleAreaHeight
-        val availableHorizontalScroll=mAvailableScrollAreaWidth-mVisibleAreaWidth
+        xv=if (abs(xv) >=MIN_FLING_VELOCITY) xv else 0
+        yv=if (abs(yv) >=MIN_FLING_VELOCITY) yv else 0
+        val scrollMaxHeight=if (mAvailableScrollAreaHeight<=mVisibleAreaHeight) 0 else mAvailableScrollAreaHeight-mVisibleAreaHeight
+        val scrollMaxWidth=if (mAvailableScrollAreaWidth<=mVisibleAreaWidth) 0 else mAvailableScrollAreaWidth-mVisibleAreaWidth
+        //we don't want to process a fling when over scrolling
         when (mScrollDirection){
-            ScrollDirection.ALL->{
-                mOverScroller.fling(scrollX,scrollY,-xv,-yv,0,availableHorizontalScroll,0,availableVerticalScroll)
-            }
             ScrollDirection.VERTICAL->{
-                mOverScroller.fling(0,scrollY,0,-yv,0,0,-10000,availableVerticalScroll)
+                if (mVerticalAlreadyOverScroll){
+                    return
+                }
+                mOverScroller.fling(0,scrollY,0,-yv,0,0,0,scrollMaxHeight)
             }
             ScrollDirection.HORIZONTAL->{
-                mOverScroller.fling(scrollX,0,-xv,0,0,availableHorizontalScroll,0,0)
+                if (mHorizontalAlreadyOverScroll){
+                    return
+                }
+                mOverScroller.fling(scrollX,0,-xv,0,0,scrollMaxWidth,0,0)
             }
         }
     }
@@ -279,8 +353,7 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
 
     private fun scheduleVerticalRestoreAnimator(){
         val shouldRestoreTo = if (scrollY<0 ||
-            (scrollY>0 && scrollY<mAvailableScrollAreaHeight-mVisibleAreaHeight) ||
-            (mAvailableScrollAreaHeight<=mVisibleAreaHeight)){
+            (scrollY>0 && mAvailableScrollAreaHeight<=mVisibleAreaHeight)){
             //restore to top
             0
         }else{
@@ -299,8 +372,7 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
     }
     private fun scheduleHorizontalRestoreAnimator(){
         val shouldRestoreTo=if (scrollX<0 ||
-            (scrollX>0 && scrollX<mAvailableScrollAreaWidth-mVisibleAreaWidth) ||
-            (mAvailableScrollAreaWidth<=mVisibleAreaWidth)){
+            (scrollX>0 && mAvailableScrollAreaWidth<=mVisibleAreaWidth)){
             //restore to top
             0
         }else{
@@ -319,12 +391,10 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
     }
 
     override fun computeScroll() {
-        super.computeScroll()
-        val compute=mOverScroller.computeScrollOffset();
+        val compute=mOverScroller.computeScrollOffset()
         if (compute){
             val scrollX=mOverScroller.currX
             val scrollY=mOverScroller.currY
-            Log.v(TAG,"Scroll to ${scrollX},${scrollY}")
             scrollTo(scrollX,scrollY)
             invalidate()
         }
@@ -340,10 +410,5 @@ class OverScrollView(context: Context,attrs:AttributeSet?,defStyle:Int,defRes:In
          * Supports horizontal scroll
          */
         HORIZONTAL(1),
-
-        /**
-         * Supports scroll in all direction
-         */
-        ALL(2)
     }
 }
